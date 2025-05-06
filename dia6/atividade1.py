b419c942f93eb0ef4f55fe578327f34411b1b9c6
AnaCarolliny26 <anacarolliny2456@gmail.com>
2025-05-06 17:45:44 -0300

    atividades 1 e 2

---
+++ dia6/atividade1.py
@@ -0,0 +1,29 @@
+# Faça um programa que solicite ao usuário os valores de v0, a e t.
+# O programa deve calcular e exibir a distância percorrida pelo objeto
+
+import sys
+
+
+velocidade_inicial = int(input('Digite o valor de v0:'))
+if velocidade_inicial <=0:
+    sys.exit ('Informe Velocidade Positiva.')
+
+
+aceleração = int(input('Digite o valor de a:'))
+if aceleração <=0:
+    sys.exit ('Informe Aceleração Positiva.')
+
+
+
+tempo = int(input('Digite o valor de t:'))
+if tempo <=0:
+    sys.exit ('Informe o tempo positivo')
+
+
+
+
+
+
+
+
+
