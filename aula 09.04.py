14fa21cfe176a24fa1369a4f8c88e08582d7c50e
AnaCarolliny26 <anacarolliny2456@gmail.com>
2025-07-09 17:20:38 -0300

    a

---
+++ aula 09.04.py
@@ -0,0 +1,26 @@
+import os, sys
+
+# Obtendo o diretório onde o programa está salvo
+strDir = os.path.dirname(__file__)
+
+# Abrindo e lendo o arquivo
+try:
+   arqLeitura = open(f'{strDir}\\carta.txt', 'r', encoding='utf-8')
+except FileNotFoundError:
+   sys.exit('\nERRO: Arquivo não encontrado...')
+except Exception as erro:
+   print(f'\nERRO: {erro}')
+
+else:
+   lstTimes = list ()
+   while True:
+       strLinha = arqLeitura.readline().strip() # Lendo a linha e armazenando na variável
+
+       if not strLinha: break # Interrompe o laço quando não há conteúdo na linha (EOF)
+
+       lstAux =  [int(i) if i.isdigit() else i for i in strLinha.split(';')]
+
+       lstTimes.append(strLinha) # Adicionando linha lida na lista
+       arqLeitura.close()
+
+print(lstTimes)
